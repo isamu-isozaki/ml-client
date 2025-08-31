e85188315cd0032671cac65413e610e55995227c
Isamu Isozaki <isamu.website@gmail.com>
2025-08-31 16:03:05 +0900

    Attempt adding index to fit pydantic

--- llm_exl2_dynamic_gen.py
+++ llm_exl2_dynamic_gen.py
@@ -426,7 +426,7 @@ async def stream_response(prompt_id, timeout=180):
             # Check for final response or timeout
             if prompt_id in responses:
                 final_response = responses.pop(prompt_id)
-                yield f'data: {{"id":"chatcmpl-{prompt_id}","object":"chat.completion.chunk","created":{int(time.time())},"model":"{repo_str}","choices":[{{"delta":{"content": ""},"finish_reason":"stop"}}]}}\n\n'
+                yield f'data: {{"id":"chatcmpl-{prompt_id}","object":"chat.completion.chunk","created":{int(time.time())},"model":"{repo_str}","choices":[{{"delta":{"index": 0, "content": ""},"finish_reason":"stop"}}]}}\n\n'
                 break
 
 
@@ -520,7 +520,9 @@ def process_prompts():
                                 "choices": [
                                     {
                                         "delta": {
-                                            "content": outcontent
+                                            "index": 0,
+                                            "content": outcontent,
+                                            "finish_reason": reason
                                         }
                                     }
                                 ]
